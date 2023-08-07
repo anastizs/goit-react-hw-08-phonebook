82d44e22d9e8cf71522bb7fecbc09c5ecd6ed769
Anastasiia <dessertkiev@gmail.com>
2023-08-07 18:05:02 +0300

    .

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,17 +1,19 @@
-import { createSelector } from "@reduxjs/toolkit";
-
-export const selectContacts = state => state.contacts.items;
-
-export const selectError = state => state.contacts.error;
-
-export const selectIsLoading = state => state.contacts.isLoading;
-
 export const selectFilter = state => state.filter;
 
-export const selectVisibleContacts = createSelector(
-    [selectContacts, selectFilter],
-    (contacts, filter) =>
-    (contacts.filter(contact =>
-        contact.name.toLowerCase().includes(filter)
-    ))
-)
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+
+export const selectIsAuth = state => state.auth.isAuth
+
+export const selectUser = state => state.auth.user;
+
+export const selectIsRefreshing = state => state.auth.isRefreshing;
+
+export const selectError = state => state.auth.error;
+
+export const selectIsDrawerOpen = state => state.modal.isDrawerOpen;
+
+export const selectIsDialogOpen = state => state.modal.isDialogOpen;
+
+export const selectDrawerVariant = state => state.modal.variant;
+
+export const selectIdToEdit = state => state.modal.idToEdit;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,43 @@
-import { configureStore} from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-import contactsReducer from "./contactsSlice";
-import filterReducer from "./filterSlice";
+import authReducer from './authSlice';
+import filterReducer from './filterSlice'
+import contactsApi from './contactsApi'
+import modalReducer from './modalSlice';
 
+const authPersistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['token'],
+};
 
 export const store = configureStore({
     reducer: {
-        contacts: contactsReducer,
-        filter: filterReducer
-    }
+        auth: persistReducer(authPersistConfig, authReducer),
+        [contactsApi.reducerPath]: contactsApi.reducer,
+        filter: filterReducer,
+        modal: modalReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }).concat(contactsApi.middleware)
 })
+
+export const persistor = persistStore(store);
+
+setupListeners(store.dispatch)
